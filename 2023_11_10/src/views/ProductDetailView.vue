177a293b1962301a54da5fc3e79da79119042484
Lim-Seunghwan <hwan7185@gmail.com>
2023-11-10 18:02:07 +0900

    commit

---
+++ 2023_11_10/src/views/ProductDetailView.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <h1>상세 페이지</h1>
+        <div v-if="product" class="product-card">
+            <img :src="product.image" alt="" class="product-card img">
+            <strong>{{ product.title }}</strong>
+            <p>가격 : ${{ product.price }}</p>
+        </div>
+        <div v-else>
+            <strong>로딩 중 ...</strong>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { ref } from "vue"
+import { useRoute } from "vue-router";
+import axios from "axios"
+
+const route = useRoute()
+const product = ref("")
+const productId = route.params.productId
+const storeURL = `https://fakestoreapi.com/products/${productId}`
+
+axios.get(storeURL)
+    .then((response) => {
+        console.log(response.data)
+        product.value = response.data
+    })
+    .catch((error) => {
+        console.error(error)
+    })
+</script>
+
+<style scoped>
+.product-card {
+    border: 1px solid black;
+    width: 200px;
+}
+
+.product-card img{
+    width: 200px;
+    height: 200px;
+    object-fit: fill;
+}
+</style>
